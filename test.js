94546baf11569d240a5e0c3a8b53db163112db1c
Charlie Gordon <cwgordon7@157412.no-reply.drupal.org>
2009-09-04 15:05:13 +0000

    Allow QUnit to POST tests back to PHP/SimpleTest.

--- test.js
+++ test.js
@@ -26,8 +26,26 @@ Drupal.behaviors.runTests = {
       var testCase = Drupal.tests[index];
       var info = testCase.getInfo();
       module(info.group, testCase);
-      test(info.name, testCase.test);
+      test(info.name + '<span style="display: none;">' + info.description + '</span>', testCase.test);
     }
+
+    // After we've finished running all of them, they should be on the page, so
+    // send a post request back to Drupal.
+    setTimeout(function() {
+      $('#tests').find('> li').each(function() {
+        data = {};
+        data.group = $(this).find('> strong').get(0).innerHTML.match(/(.*) module: <span>/)[1];
+        data.name = $(this).find('> strong > span').get(0).innerHTML.match(/(.*)<span style="display: none;">/)[1];
+        data.description = $(this).find('> strong > span').get(0).innerHTML.match(/<span style="display: none;">(.*)<\/span>/)[1];
+        index = 0;
+        $(this).find('> ol > li').each(function() {
+          data['assertions[' + index + '][status]'] = $(this).hasClass('pass');
+          data['assertions[' + index + '][message]'] = this.innerHTML;
+          index++;
+        });
+        $.post(Drupal.settings.basePath + '?q=simpletest/javascript-callback', data);
+      });
+    }, 100);
   }
 };
 
